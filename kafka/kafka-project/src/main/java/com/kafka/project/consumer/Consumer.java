5a05fa4a483cb7c2bd21584f67b5e979fc52da77
Radosław Burdziński <radoslaw.burdzinski@beone.pl>
2023-03-18 20:49:28 +0100

    influx chronograf added for kafka

---
+++ kafka/kafka-project/src/main/java/com/kafka/project/consumer/Consumer.java
@@ -0,0 +1,17 @@
+package com.kafka.project.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class Consumer {
+
+
+    @KafkaListener(id = "topic1", topics = "topic1")
+    public void listen(String in) {
+        System.out.println("AAAAAAAAAAAAAAAAAAA    "+in+"AAAAAAAAAAAAAAAAAAA    ");
+    }
+
+}
--- kafka/kafka-project/src/main/java/com/kafka/project/consumer/ConsumerApp.java
+++ kafka/kafka-project/src/main/java/com/kafka/project/consumer/ConsumerApp.java
@@ -1,11 +1,8 @@
 package com.kafka.project.consumer;
 
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 public class ConsumerApp {
@@ -13,11 +10,4 @@ public class ConsumerApp {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApp.class, args);
     }
-
-
-    @KafkaListener(id = "topic1", topics = "topic1")
-    public void listen(String in) {
-        System.out.println("AAAAAAAAAAAAAAAAAAA    "+in+"AAAAAAAAAAAAAAAAAAA    ");
-    }
-
 }
